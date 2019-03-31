cf38ec4dd0b5385d0e8ba9e5fa8ecab0ba073a91
CheezItMan <chris@adadevelopersacademy.org>
2019-03-30 23:18:57 -0700

    convert from xdescribe to skip

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "minitest/skip_dsl"
 
 describe TasksController do
   let (:task) {
@@ -25,85 +26,93 @@ describe TasksController do
     end
   end
 
-  # Uncomment these tests for Wave 2
-  # describe "show" do
-  #   it "can get a valid task" do
-  #     # Act
-  #     get task_path(task.id)
+  # Unskip these tests for Wave 2
+  describe "show" do
+    it "can get a valid task" do
+      skip
+      # Act
+      get task_path(task.id)
 
-  #     # Assert
-  #     must_respond_with :success
-  #   end
+      # Assert
+      must_respond_with :success
+    end
 
-  #   it "will redirect for an invalid task" do
-  #     # Act
-  #     get task_path(-1)
+    it "will redirect for an invalid task" do
+      skip
+      # Act
+      get task_path(-1)
 
-  #     # Assert
-  #     must_respond_with :redirect
-  #     expect(flash[:error]).must_equal "Could not find task with id: -1"
-  #   end
-  # end
+      # Assert
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "Could not find task with id: -1"
+    end
+  end
 
-  # describe "new" do
-  #   it "can get the new task page" do
-  #     # Act
-  #     get new_task_path
+  describe "new" do
+    skip
+    it "can get the new task page" do
+      # Act
+      get new_task_path
 
-  #     # Assert
-  #     must_respond_with :success
-  #   end
-  # end
+      # Assert
+      must_respond_with :success
+    end
+  end
 
-  # describe "create" do
-  #   it "can create a new task" do
-  #     # Arrange
-  #     task_hash = {
-  #       task: {
-  #         name: "new task",
-  #         description: "new task description",
-  #         completion_date: nil,
-  #       },
-  #     }
+  describe "create" do
+    skip
+    it "can create a new task" do
+      # Arrange
+      task_hash = {
+        task: {
+          name: "new task",
+          description: "new task description",
+          completion_date: nil,
+        },
+      }
 
-  #     # Act-Assert
-  #     expect {
-  #       post tasks_path, params: task_hash
-  #     }.must_change "Task.count", 1
+      # Act-Assert
+      expect {
+        post tasks_path, params: task_hash
+      }.must_change "Task.count", 1
 
-  #     new_task = Task.find_by(name: task_hash[:task][:name])
-  #     expect(new_task.description).must_equal task_hash[:task][:description]
-  #     expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
-  #     expect(new_task.completed).must_equal task_hash[:task][:completed]
+      new_task = Task.find_by(name: task_hash[:task][:name])
+      expect(new_task.description).must_equal task_hash[:task][:description]
+      expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
+      expect(new_task.completed).must_equal task_hash[:task][:completed]
 
-  #     must_respond_with :redirect
-  #     must_redirect_to task_path(new_task.id)
-  #   end
-  # end
+      must_respond_with :redirect
+      must_redirect_to task_path(new_task.id)
+    end
+  end
+
+  # Unskip and complete these tests for Wave 3
+  describe "edit" do
+    it "can get the edit page for an existing task" do
+      skip
+      # Your code here
+    end
+
+    it "will respond with redirect when attempting to edit a nonexistant task" do
+      skip
+      # Your code here
+    end
+  end
 
   # Uncomment and complete these tests for Wave 3
-  # describe "edit" do
-  #   it "can get the edit page for an existing task" do
-  #     # Your code here
-  #   end
+  describe "update" do
+    # Note:  If there was a way to fail to save the changes to a task, that would be a great
+    #        thing to test.
+    it "can update an existing task" do
+      skip
+      # Your code here
+    end
 
-  #   it "will respond with redirect when attempting to edit a nonexistant task" do
-  #     # Your code here
-  #   end
-  # end
-
-  # Uncomment and complete these tests for Wave 3
-  # describe "update" do
-  #   # Note:  If there was a way to fail to save the changes to a task, that would be a great
-  #   #        thing to test.
-  #   it "can update an existing task" do
-  #     # Your code here
-  #   end
-
-  #   it "will redirect to the root page if given an invalid id" do
-  #     # Your code here
-  #   end
-  # end
+    it "will redirect to the root page if given an invalid id" do
+      skip
+      # Your code here
+    end
+  end
 
   # Complete these tests for Wave 4
   describe "destroy" do
