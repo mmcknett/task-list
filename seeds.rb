7989d77b12ee46cb0956d3cda9e30c51f9355e42
CheezItMan <chris@adadevelopersacademy.org>
2019-04-04 11:41:29 -0700

    ran rufo

--- seeds.rb
+++ seeds.rb
@@ -3,16 +3,16 @@ def random_time
 end
 
 tasks = [
-  { name: "The First Task", description: "", completed_at: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed_at: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed_at: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed_at: random_time },
-  { name: "Plant Flowers", description: "", completed_at: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed_at: random_time }
+  {name: "The First Task", description: "", completed_at: random_time},
+  {name: "Go to Brunch", description: ""},
+  {name: "Go to Lunch", description: "", completed_at: random_time},
+  {name: "Go to Second Lunch", description: ""},
+  {name: "Play Video Games", description: "", completed_at: random_time},
+  {name: "High Five Somebody You Don't Know", description: "", completed_at: random_time},
+  {name: "Plant Flowers", description: "", completed_at: random_time},
+  {name: "Call Mom", description: ""},
+  {name: "She worries, you know.", description: ""},
+  {name: "Nap.", description: "", completed_at: random_time},
 ]
 
 tasks.each do |task|
